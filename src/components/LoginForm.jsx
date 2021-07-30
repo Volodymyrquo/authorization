3832c006fca107ebd0267740f87f948311df7bc6
marcelo <develop@dev>
2021-07-30 12:50:28 +0300

    loginForm added logic and styles

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,11 +1,123 @@
-import React from 'react'
+import React, {useState, useEffect } from 'react';
+import  '../assets/scss/index.scss';
+import 'react-phone-number-input/style.css'
+import PhoneInput from 'react-phone-number-input'
+import { isMobile } from "react-device-detect";
+// import socialLinks from '../../common/data/socialLinks';
+import { useHistory, useLocation } from "react-router-dom";
+// import { sendPhone } from '../../api/api';
+import sendIcon from '../assets/images/send.svg';
 
-export const LoginForm = () => {
+export const LoginForm = ({
+    goToAuthPage = () => history.replace({
+        pathname: '/confirmcode',
+        state: {
+            prevPath: location.pathname,
+        }
+    }),
+    sendPhone = () => console.log("Pass api request method to send phone number"), 
+    title = 'Join',
+    socialLinks = [],
+}) => {
+    const [countryCode, setCountryCode] = useState();
+    const [number, setNumber] = useState('');
+    const history = useHistory();
+    const location = useLocation();
+    
+    const goToVeryfycationCodePage = (e) => {
+        const messenger = e.target.alt;
+        const href = e.target.parentElement.href;
+        localStorage.setItem("messenger", messenger);
+        localStorage.setItem("href", href);
+    };
+
+    const goToConfirmForm = () => history.replace({
+        pathname: '/confirmcode',
+        state: {
+            prevPath: location.pathname,
+        }
+    });
+
+    const submitPhoneNumber = (event) => {
+        event.preventDefault();
+
+        if (!number) {
+            return;
+        }
+
+        let code = countryCode;
+        let phoneNumber = code + number;
+
+        localStorage.setItem("messenger", phoneNumber);
+        localStorage.setItem("href", phoneNumber);
+
+        phoneNumber = phoneNumber.replace("+", "");
+
+        sendPhone();
+        // sendPhone({
+        //     phone_number: phoneNumber,
+        //     app_uid: "meet.sumra.web",
+        // }).then(
+        //     (response) => console.log(response),
+        //     (error) => console.error
+        // );
+
+        goToAuthPage();
+    };
+
+    useEffect(() => {
+        localStorage.removeItem("confirm-refresh");
+        localStorage.removeItem("create-refresh");
+    }, [])
+
     return (
-        <div>
-            <h1>LoginForm</h1>
-        </div>
+           
+        <>
+            <form className="join__form" onSubmit={submitPhoneNumber}>
+                <div className="join__form-wrap">
+                <h3 className="join__form-title">{title}</h3>
+                <h4 className="join__form-text">Join by using your Messenger</h4>
+                <ul className="join__form__list-messangers">
+                    {socialLinks.map((v, index) => {
+                        let href = "";
+
+                        if(isMobile) {
+                            href = v.hrefMobile;
+                        }
+                        else {
+                            href = v.href;
+                        }
+
+                        return (
+                            <li key={index} onClick={goToVeryfycationCodePage}>
+                                <a href={href} target="_blank" rel="noreferrer">
+                                    <img onClick={() => goToAuthPage()}  src={v.image} width={46} alt={v.title}/>
+                                </a>
+                            </li>
+                        )
+                    })}
+                </ul>
+                <h4 className="join__form__form-text">Join by using your Mobile phone</h4>
+                <div className="join__form__input-wrapper">
+                    <PhoneInput
+                        readOnly
+                        international
+                        countryCallingCodeEditable={false}
+                        defaultCountry="RU"
+                        value={countryCode}
+                        onChange={(e) => {
+                            setCountryCode(e)
+                        }}
+                    />
+                    
+                    <span></span>
+                    <input onChange={(e) => setNumber(e.target.value)} value={number} className="join__form__input" placeholder={'Enter phone number'} />
+                    <button type="submit">
+                        <img alt="icon" src={sendIcon} />
+                    </button>
+                </div>
+                </div>
+            </form>)
+        </>
     )
 }
-
-// export default LoginForm;
